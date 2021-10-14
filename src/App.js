4c2ffbbfdc8109a67f16383d274dad721b15cd34
Amy <e.crescentini87@gmail.com>
2021-10-14 11:15:59 +0200

    Project begin

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Work in Progress... </h1>
+      <div className="spin1"></div>
+    </div>
+  );
+}
