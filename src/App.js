f0d7fae612788a5274ade4a5982bb271ee7b6b7e
Amy <e.crescentini87@gmail.com>
2021-10-14 15:18:16 +0200

    Spinner created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Spin from "./Spin";
 
 export default function App() {
   return (
     <div className="container">
       <h1>Work in Progress... </h1>
-      <div className="spin1"></div>
+      <Spin/>
     </div>
   );
 }
---
+++ src/Spin.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./App.css";
+export default function Spin(){
+    return <div className="spin1"></div>;
+}
